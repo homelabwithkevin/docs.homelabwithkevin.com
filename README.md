e9667805686f91f9b5a46984cada6eb06865c793
Kevin <116416787+homelabwithkevin@users.noreply.github.com>
2024-04-21 22:48:13 +0000

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # docs.homelabwithkevin.com
-docs.homelabwithkevin.com
+
+[![Built with Starlight](https://astro.badg.es/v2/built-with-starlight/tiny.svg)](https://starlight.astro.build)
---
+++ astro.config.mjs
@@ -0,0 +1,26 @@
+import { defineConfig } from 'astro/config';
+import starlight from '@astrojs/starlight';
+
+// https://astro.build/config
+export default defineConfig({
+	integrations: [
+		starlight({
+			title: 'Docs',
+			social: {
+				github: 'https://github.com/homelabwithkevin/docs.homelabwithkevin.com',
+			},
+			sidebar: [
+				{
+					label: 'Intro',
+					items: [
+						// Each item here is one entry in the navigation menu.
+						{
+							label: 'Welcome',
+							link: '/welcome'
+						},
+					],
+				}
+			],
+		}),
+	],
+});
